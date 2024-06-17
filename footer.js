e1a92ae46d8e174b37e153a47d773fe89b8dd84e
Yash Gupta <yashguptaji2001@gmail.com>
2024-06-17 18:09:16 +0530

    update 2 fixed

--- footer.js
+++ footer.js
@@ -2,17 +2,11 @@ document.write(`<div class="container-fluid bg-dark text-light mt-5 wow fadeInUp
         <div class="container">
             <div class="row gx-5">
                 <div class="col-lg-4 col-md-6 footer-about">
-                    <div class="d-flex flex-column align-items-center justify-content-center text-center h-100 bg-primary p-4">
+                    <div class="d-flex  align-items-center justify-content-center text-center h-100 bg-primary p-4">
                         <a href="index.html" class="navbar-brand">
                             <h1 class="m-0 text-white"><img src="img/logo.png" width=100% height=100%></h1>
                         </a>
-                        <p class="mt-3 mb-4">Never miss our latest insights, news and announcements. Subscribe now to get updates delivered straight to your inbox.</p>
-                        <form action="">
-                            <div class="input-group">
-                                <input type="text" class="form-control border-white p-3" placeholder="Your Email">
-                                <button class="btn btn-dark">Sign Up</button>
-                            </div>
-                        </form>
+                        
                     </div>
                 </div>
                 <div class="col-lg-8 col-md-6">
@@ -33,12 +27,7 @@ document.write(`<div class="container-fluid bg-dark text-light mt-5 wow fadeInUp
                                 <i class="bi bi-telephone text-primary me-2"></i>
                                 <p class="mb-0">+91 9810 625 129</p>
                             </div>
-                            <div class="d-flex mt-4">
-                                <a class="btn btn-primary btn-square me-2" href="#"><i class="fab fa-twitter fw-normal"></i></a>
-                                <a class="btn btn-primary btn-square me-2" href="#"><i class="fab fa-facebook-f fw-normal"></i></a>
-                                <a class="btn btn-primary btn-square me-2" href="#"><i class="fab fa-linkedin-in fw-normal"></i></a>
-                                <a class="btn btn-primary btn-square" href="#"><i class="fab fa-instagram fw-normal"></i></a>
-                            </div>
+                            
                         </div>
                         <div class="col-lg-4 col-md-12 pt-0 pt-lg-5 mb-5">
                             <div class="section-title section-title-sm position-relative pb-3 mb-4">
@@ -48,6 +37,7 @@ document.write(`<div class="container-fluid bg-dark text-light mt-5 wow fadeInUp
                                 <a class="text-light mb-2" href="service.html#consultancy"><i class="bi bi-arrow-right text-primary me-2"></i>Consulting</a>
                                 <a class="text-light mb-2" href="service.html#audit"><i class="bi bi-arrow-right text-primary me-2"></i>Audit</a>
                                 <a class="text-light mb-2" href="service.html#training"><i class="bi bi-arrow-right text-primary me-2"></i>Training</a>
+                                <a class="text-light mb-2" href="service.html#process"><i class="bi bi-arrow-right text-primary me-2"></i>Process Strengthening</a>
 
                             </div>
                         </div>
@@ -58,8 +48,8 @@ document.write(`<div class="container-fluid bg-dark text-light mt-5 wow fadeInUp
                             <div class="link-animated d-flex flex-column justify-content-start">
                                 <a class="text-light mb-2" href="index.html"><i class="bi bi-arrow-right text-primary me-2"></i>Home</a>
                                 <a class="text-light mb-2" href="about.html"><i class="bi bi-arrow-right text-primary me-2"></i>About Us</a>
-                                <a class="text-light mb-2" href="services.html"><i class="bi bi-arrow-right text-primary me-2"></i>Our Services</a>
-                                <a class="text-light mb-2" href="team.html"><i class="bi bi-arrow-right text-primary me-2"></i>Meet The Team</a>
+                                <a class="text-light mb-2" href="service.html"><i class="bi bi-arrow-right text-primary me-2"></i>Our Services</a>
+                               <!-- <a class="text-light mb-2" href="team.html"><i class="bi bi-arrow-right text-primary me-2"></i>Meet The Team</a> -->   
                                 <a class="text-light" href="contact.html"><i class="bi bi-arrow-right text-primary me-2"></i>Contact Us</a>
                             </div>
                         </div>
